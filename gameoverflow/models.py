cb65264aa62643b254e078e558d6cd7e950de754
dev-rome <haynesjerome@gmail.com>
2022-07-02 19:49:19 -0400

    added question and answer models

--- gameoverflow/models.py
+++ gameoverflow/models.py
@@ -1,3 +1,28 @@
+from multiprocessing.dummy import Array
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
+from django.contrib.auth.models import User
 
 # Create your models here.
+# quesion model
+class Question(models.Model):
+    question_title = models.CharField(max_length=200)
+    question_text = models.CharField(max_length=200)
+    question_tags = ArrayField(models.CharField(max_length=200), size=5)
+    question_vote = models.IntegerField(default=0)
+    question_views = models.IntegerField(default=0)
+    pub_date = models.DateTimeField('date published')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+
+    def __str__(self):
+        return self.question_text
+
+class Answer(models.Model):
+    answer_text = models.CharField(max_length=200)
+    answer_vote = models.IntegerField(default=0)
+    answer_question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField('date published')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+
+    def __str__(self):
+        return self.answer_text        
