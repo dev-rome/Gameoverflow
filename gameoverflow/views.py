538f62d710d03b9373c60e5f976ad7a3605ea746
dev-rome <haynesjerome@gmail.com>
2022-07-03 21:43:47 -0400

    created question list view

--- gameoverflow/views.py
+++ gameoverflow/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Question, Answer
 
 # Create your views here.
 def index(request):
     return render(request, 'gameoverflow/index.html')
+
+def question_list(request):
+    questions = Question.objects.all()
+    return render(request, 'gameoverflow/question_list.html', {'questions': questions})    
