721bce78bcc0f1b0d591b1d56dafba7c202bce56
dev-rome <haynesjerome@gmail.com>
2022-07-05 04:33:11 -0400

    created path for account sign-up

--- gameoverflow/urls.py
+++ gameoverflow/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('question_list/', views.question_list, name='question_list'),
+    path('accounts/signup/', views.Signup.as_view(), name="signup")
 ]
