df60a87fe050638d8fb1572161c96f9e23a75dfe
dev-rome <haynesjerome@gmail.com>
2022-07-05 04:35:32 -0400

    created sign-up view

--- gameoverflow/views.py
+++ gameoverflow/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.views import View
 from .models import Question, Answer
 
 # Create your views here.
@@ -7,4 +10,20 @@ def index(request):
 
 def question_list(request):
     questions = Question.objects.all()
-    return render(request, 'gameoverflow/question_list.html', {'questions': questions})    
+    return render(request, 'gameoverflow/question_list.html', {'questions': questions})
+
+class Signup(View):
+    def get(self, request):
+        form = UserCreationForm()
+        context = {"form": form}
+        return render(request, 'registration/signup.html', context)
+
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+        else:
+            context = {"form": form}
+            return render(request, 'registration/signup.html', context)        
