500a6567694ec20d4dd66a407611646974006c63
dev-rome <haynesjerome@gmail.com>
2022-07-05 20:46:22 -0400

    added urls

--- gameoverflow/urls.py
+++ gameoverflow/urls.py
@@ -4,5 +4,11 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('question_list/', views.question_list, name='question_list'),
+    path('question_detail/<int:pk>/', views.question_detail, name='question_detail'),
+    path('question_ask/', views.question_ask, name='question_ask'),
+    # path('question/<int:pk>/edit/', views.question_edit, name='question_edit'),
+    # path('question/<int:pk>/delete/', views.question_delete, name='question_delete'),
+    # path('answer/<int:pk>/edit/', views.answer_edit, name='answer_edit'),
+    # path('answer/<int:pk>/delete/', views.answer_delete, name='answer_delete'),
     path('accounts/signup/', views.Signup.as_view(), name="signup")
 ]
