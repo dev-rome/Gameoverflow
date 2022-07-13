bb021494110222e8e4e0faad9ad014159572feea
dev-rome <haynesjerome@gmail.com>
2022-07-12 23:46:04 -0400

    added edit and delete views

--- gameoverflow/urls.py
+++ gameoverflow/urls.py
@@ -6,9 +6,9 @@ urlpatterns = [
     path('question_list/', views.question_list, name='question_list'),
     path('question_detail/<int:id>/', views.question_detail, name='question_detail'),
     path('question_ask/', views.question_ask, name='question_ask'),
-    # path('question/<int:pk>/edit/', views.question_edit, name='question_edit'),
-    # path('question/<int:pk>/delete/', views.question_delete, name='question_delete'),
-    # path('answer/<int:pk>/edit/', views.answer_edit, name='answer_edit'),
-    # path('answer/<int:pk>/delete/', views.answer_delete, name='answer_delete'),
+    path('question_detail/<int:id>/edit', views.question_edit, name='question_edit'),
+    path('question/<int:id>/delete', views.question_delete, name='question_delete'),
+    path('answer/<int:id>/edit', views.answer_edit, name='answer_edit'),
+    path('answer/<int:id>/delete', views.answer_delete, name='answer_delete'),
     path('accounts/signup/', views.Signup.as_view(), name="signup")
 ]
